3c6f41d8292e8365f2e7821d4c0f8e61039fec30
Chao Wu <chao.wu@aquariux.com>
2022-05-09 10:18:19 +0800

    Change the treesitter language parser to `all` as the parser of `maintained` is deprecated; Adding tag akinsho/bufferline and akinsho/toggleterm packer management

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -54,10 +54,11 @@ return packer.startup(function(use)
   }) -- Easily comment stuff
   use("kyazdani42/nvim-web-devicons")
   use("kyazdani42/nvim-tree.lua")
-  use("akinsho/bufferline.nvim")
+  -- Tagging is required for installing the latest bufferline
+  use({"akinsho/bufferline.nvim", tag='v2.*', requires = 'kyazdani42/nvim-web-devicons'})
+  use({"akinsho/toggleterm.nvim", tag='v1.*'})
   use("moll/vim-bbye")
   use("nvim-lualine/lualine.nvim")
-  use("akinsho/toggleterm.nvim")
   use("ahmedkhalf/project.nvim")
   use("lewis6991/impatient.nvim")
   use("lukas-reineke/indent-blankline.nvim")
