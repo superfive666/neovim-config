848a095bf723e45cb93bcea613c8d45fd7034600
Wu chao <chao.wu@bitdeer.com>
2022-10-19 14:21:10 +0800

    Fixing the bufferline configuration lua file

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -6,6 +6,7 @@ end
 
 bufferline.setup({
 	options = {
+    mode="buffers",
 		numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
 		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
 		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
@@ -14,7 +15,10 @@ bufferline.setup({
 		-- NOTE: this plugin is designed with this icon in mind,
 		-- and so changing this is NOT recommended, this is intended
 		-- as an escape hatch for people who cannot bear it for whatever reason
-		indicator_icon = "▎",
+    indicator = {
+      icon = "▎", -- this should be omitted if indicator style is not 'icon'
+      style = "icon"
+    },
 		-- buffer_close_icon = "",
 		buffer_close_icon = '',
 		modified_icon = "●",
@@ -35,11 +39,44 @@ bufferline.setup({
 		max_name_length = 30,
 		max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
 		tab_size = 21,
-		diagnostics = false, -- | "nvim_lsp" | "coc",
-		diagnostics_update_in_insert = false,
-		-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-		--   return "("..count..")"
-		-- end,
+		-- diagnostics = false, -- | "nvim_lsp" | "coc",
+		diagnostics = "nvim_lsp",
+		diagnostics_update_in_insert = true,
+    --- count is an integer representing total count of errors
+    --- level is a string "error" | "warning"
+    --- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
+    --- this should return a string
+    --- Don't get too fancy as this function will be executed a lot
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      if context.buffer:current() then
+        local erro_count = diagnostics_dict["error"] or 0
+        local warn_count = diagnostics_dict["warning"] or 0
+        local hint_count = diagnostics_dict["hint"] or 0
+        local info_count = diagnostics_dict["info"] or 0
+
+        local area_message = count .. ":"
+
+        if erro_count ~= 0 then
+          area_message = area_message .. "  " .. erro_count
+        end
+
+        if warn_count ~= 0 then
+          area_message = area_message .. "  " .. warn_count
+        end
+
+        if hint_count ~= 0 then
+          area_message = area_message .. "  " .. hint_count
+        end
+
+        if info_count ~= 0 then
+          area_message = area_message .. "  " .. info_count
+        end
+
+        return area_message
+      end
+
+      return ""
+    end,
 		-- NOTE: this will be called a lot so don't do any heavy processing here
 		-- custom_filter = function(buf_number)
 		--   -- filter out filetypes you don't want to see
@@ -64,131 +101,40 @@ bufferline.setup({
 		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
 		-- can also be a table containing 2 custom separators
 		-- [focused and unfocused]. eg: { '|', '|' }
-		separator_style = "triangular", -- | "thick" | "thin" | { 'any', 'any' },
+		separator_style = "padded_slant", -- | "thick" | "thin" | { 'any', 'any' },
+    separators = {"|", "|"},
 		enforce_regular_tabs = true,
 		always_show_bufferline = true,
 		-- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
 		--   -- add custom logic
 		--   return buffer_a.modified > buffer_b.modified
+    custom_areas = {
+      right = function()
+        local result = {}
+        local seve = vim.diagnostic.severity
+        local error = #vim.diagnostic.get(0, {severity = seve.ERROR})
+        local warning = #vim.diagnostic.get(0, {severity = seve.WARN})
+        local info = #vim.diagnostic.get(0, {severity = seve.INFO})
+        local hint = #vim.diagnostic.get(0, {severity = seve.HINT})
+
+        if error ~= 0 then
+          table.insert(result, { text = "  " .. error, fg = "#EC5241" })
+        end
+
+        if warning ~= 0 then
+          table.insert(result, { text = "  " .. warning, fg = "#EFB839" })
+        end
+
+        if hint ~= 0 then
+          table.insert(result, { text = "  " .. hint, fg = "#A3BA5E" })
+        end
+
+        if info ~= 0 then
+          table.insert(result, { text = "  " .. info, fg = "#7EA9A7" })
+        end
+        return result
+      end,
+    }
 		-- end
-	},
-	custom_areas = {
-		right = function()
-			local result = {}
-			local error = vim.lsp.diagnostic.get_count(0, [[Error]])
-			local warning = vim.lsp.diagnostic.get_count(0, [[Warning]])
-			local info = vim.lsp.diagnostic.get_count(0, [[Information]])
-			local hint = vim.lsp.diagnostic.get_count(0, [[Hint]])
-
-			if error ~= 0 then
-				table.insert(result, { text = "  " .. error, fg = "#EC5241" })
-			end
-
-			if warning ~= 0 then
-				table.insert(result, { text = "  " .. warning, fg = "#EFB839" })
-			end
-
-			if hint ~= 0 then
-				table.insert(result, { text = "  " .. hint, fg = "#A3BA5E" })
-			end
-
-			if info ~= 0 then
-				table.insert(result, { text = "  " .. info, fg = "#7EA9A7" })
-			end
-			return result
-		end,
-	},
-	highlights = {
-		fill = {
-			fg = { attribute = "fg", highlight = "#ff0000" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		background = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-
-		-- buffer_selected = {
-		--   fg = {attribute='fg',highlight='#ff0000'},
-		--   bg = {attribute='bg',highlight='#0000ff'},
-		--   gui = 'none'
-		--   },
-		buffer_visible = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-
-		close_button = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		close_button_visible = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		-- close_button_selected = {
-		--   fg = {attribute='fg',highlight='TabLineSel'},
-		--   bg ={attribute='bg',highlight='TabLineSel'}
-		--   },
-
-		tab_selected = {
-			fg = { attribute = "fg", highlight = "Normal" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
-		tab = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		tab_close = {
-			-- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
-			fg = { attribute = "fg", highlight = "TabLineSel" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
-
-		duplicate_selected = {
-			fg = { attribute = "fg", highlight = "TabLineSel" },
-			bg = { attribute = "bg", highlight = "TabLineSel" },
-      italic = true
-		},
-		duplicate_visible = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-      italic = true
-		},
-		duplicate = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-      italic = true
-		},
-
-		modified = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		modified_selected = {
-			fg = { attribute = "fg", highlight = "Normal" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
-		modified_visible = {
-			fg = { attribute = "fg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-
-		separator = {
-			fg = { attribute = "bg", highlight = "TabLine" },
-			bg = { attribute = "bg", highlight = "TabLine" },
-		},
-		separator_selected = {
-			fg = { attribute = "bg", highlight = "Normal" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
-		-- separator_visible = {
-		--   fg = {attribute='bg',highlight='TabLine'},
-		--   bg = {attribute='bg',highlight='TabLine'}
-		--   },
-		indicator_selected = {
-			fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
-			bg = { attribute = "bg", highlight = "Normal" },
-		},
 	},
 })
